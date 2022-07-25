1ba7ac3ba8e616c65854b2c952f1b59fe077da4a
prasanthprabakaran <iprasanthpraba@gmail.com>
2022-07-25 14:40:23 +0530

    email

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 import express from "express";
-// import { MongoClient } from "mongodb";
+import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import cors from 'cors';
 import connectDB from './config/db.js'
 import errorHandler from './middleware/error.js' 
 import {listRouter} from './routes/auth.js'
 import {privateRouter} from './routes/private.js'
+import { dataRouter } from './routes/data.js'
 
 
 dotenv.config();
 connectDB();
+
+const MONGO_URL = process.env.MONGO_URL;
+async function createConnection(){
+    const client = new MongoClient(MONGO_URL);
+    await client.connect();
+    console.log("Mongo is connected 😊");
+    return client;
+}
+export const client = await createConnection();
 
 const app = express();
 const PORT = process.env.PORT;
@@ -23,6 +33,7 @@ app.get('/',(req,res,next) => {
 // Connecting Routes
 app.use('/api/V2/auth', listRouter);
 app.use('/api/V2/private', privateRouter);
+app.use('/api/V2/data', dataRouter);
 
 // Error handler Middleware
 app.use(errorHandler);

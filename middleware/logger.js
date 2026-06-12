20f5f53e742033a98967da88fd92a2e5918c3c21
Prasanth <60423798+prasanthprabakaran@users.noreply.github.com>
2026-06-12 17:20:58 +0530

    fix: correct req.method typo in logger middleware

--- middleware/logger.js
+++ middleware/logger.js
@@ -25,7 +25,7 @@ export const logEvents = async (message, logFileName) => {
 }
 
 export const logger = (req,res,next) => {
-    logEvents(`${req.methods}\t${req.url}\t${req.headers.origin}`,'reqLog.log')
+    logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`,'reqLog.log')
     console.log(`${req.method} ${req.path}`)
     next()
 }

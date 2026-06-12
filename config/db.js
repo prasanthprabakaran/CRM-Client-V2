8e7f37c57d4129d7248e4cea90d8e4287587e8f6
Prasanth <60423798+prasanthprabakaran@users.noreply.github.com>
2026-06-12 18:04:59 +0530

    removed useNewUrlParser and useUnifiedTopology as they are not needed in mongoose 9

--- config/db.js
+++ config/db.js
@@ -3,10 +3,7 @@ import mongoose from "mongoose";
 // const MONGO_URL = process.env.MONGO_URL;
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URL);
 
     console.log("MongoDB is Connected 👍😊");
   } catch (err) {

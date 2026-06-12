33aca74e8074b5ef174be119f83e03e128f1b4fc
Prasanth <60423798+prasanthprabakaran@users.noreply.github.com>
2026-06-12 17:25:57 +0530

    fix: use origin param in cors callback, fix: optionsSuccessStatus typo

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,14 +2,14 @@ import allowedOrigins from "./allowedOrigins.js";
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if(allowedOrigins.indexOf(req.header('Origin')) !== -1) {
+        if(allowedOrigins.indexOf(origin) !== -1) {
             callback(null,true)
         } else {
             callback(new Error('Not allowed by CORS'))
         }
     },
     credentials: true,
-    optionSuccessStatus: 200,
+    optionsSuccessStatus: 200,
     preflightContinue: true
     // methods: ['GET','POST','PATCH','DELETE'],
     // allowedHeaders: ['Content-Type','Authorization'],
